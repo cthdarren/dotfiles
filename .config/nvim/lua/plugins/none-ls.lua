2837790c71e1d81a8d3596a02153d8c1354933b3
cthdarren <ctze.darren@gmail.com>
2024-12-20 09:54:08 +0100

    changed prettier tab spacing to 4, bracket same line

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -7,11 +7,16 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				require("none-ls.diagnostics.eslint_d"),
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.prettier.with({
+                    extra_args = { 
+                        "--tab-width 4",
+                        "--bracket-same-line"
+
+                    }
+                }),
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.rubocop,
 			},
